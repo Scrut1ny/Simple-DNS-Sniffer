75b395c41143c84b4e482e33db1db9c023f899e1
0x00 <53458032+Scrut1ny@users.noreply.github.com>
2023-04-09 01:10:08 +0000

    Update DNS Query Sniffer.py
    few adjustments.

--- DNS Query Sniffer.py
+++ DNS Query Sniffer.py
@@ -1,6 +1,11 @@
 import subprocess
+import os
+
+# Clear the terminal
+os.system('clear')
 
 try:
+    # Check if scapy is installed, if not install it silently
     from scapy.all import *
 except ImportError:
     subprocess.call(['pip', 'install', '-qy', 'scapy'])
@@ -9,10 +14,13 @@ except ImportError:
 
 def dns_sniffer(pkt):
     if pkt.haslayer(DNS) and pkt.getlayer(DNS).qr == 0:
+        # Print DNS query request in light blue and strip the trailing dot
         print(f"DNS Query Request: \033[94m{pkt.getlayer(DNS).qd.qname.decode().strip('.')}\033[m")
 
 
 try:
+    # Start sniffing on UDP port 53 for DNS traffic
     sniff(filter="udp port 53", prn=dns_sniffer)
 except KeyboardInterrupt:
-    print("Exiting...")
+    # Exit gracefully if the user hits Ctrl+C
+    print('\nExiting...')
